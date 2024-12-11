a6f689be33a0e7c66c328a2530f4e6864e5b4f3a
Ruben Bañaga <80807840+J4YM@users.noreply.github.com>
2024-12-11 13:49:33 +0800

    Backend of User Side

---
+++ restoseatreserve/src/employee/backend/credsBackend.java
@@ -0,0 +1,78 @@
+package employee.backend;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author james
+ */
+public class credsBackend {
+    public List<employee> getAll() {
+        List<employee> items = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new FileInputStream("DATABASE/reservedatabase.txt"))) {
+            while (scanner.hasNextLine()) {
+                String itemLine = scanner.nextLine();
+
+                String itemInfo[] = itemLine.split(",");
+
+                employee item = new employee(itemInfo[0], itemInfo[1],
+                        itemInfo[2], itemInfo[3], itemInfo[4]);
+
+                items.add(item);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(backend.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return items;
+    }
+    
+    public synchronized boolean checkCreds(String name, String pass) {
+
+        List<employee> itemList = getAll();
+
+        int indexToBeDeleted = -1;
+        // find the cred to log in
+        for (int i = 0; i < itemList.size(); i++) {
+            employee item = itemList.get(i);
+
+            if (item.getName().equalsIgnoreCase(name) && item.getID().equalsIgnoreCase(pass)) {
+                indexToBeDeleted = i;
+                
+                
+            }
+        }
+        List<employee> items = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new FileInputStream("DATABASE/reservedatabase.txt"))) {
+            while (scanner.hasNextLine()) {
+                String itemLine = scanner.nextLine();
+
+                String itemInfo[] = itemLine.split(",");
+
+                employee item = new employee(itemInfo[0], itemInfo[1],
+                        itemInfo[2], itemInfo[3], itemInfo[4]);
+
+                items.add(item);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(backend.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (indexToBeDeleted == -1) {
+            return false;
+        }
+        
+        
+
+        return true;
+    }
+}
